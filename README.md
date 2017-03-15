9ea9589bf10101191525d66307e4b1fdb4b678b6
aalonsog <aalonsog@dit.upm.es>
2017-03-15 15:17:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+## Restaurants REST server
+
+### Dependencies
+
+- node.js
+- npm
+
+### How to install
+
+<pre>
+git clone https://github.com/aalonsog/BSDT_rest.git
+cd BSDT_rest
+npm install
+</pre>
+
+### How to configure
+
++ The server is configured to listen in port 8000. You can modify that folder using the env variable `PORT`
++ The server is configured to connect to database in `mongodb://localhost:27017/bio_db`. You can modify that folder using the env variable `DATABASE`
+
+Example:
+<pre>
+export PORT=8081
+</pre>
+
+### How to run
+
+<pre>
+npm start
+</pre>
+
+### How to test
+
+You can use any REST client to test the API. A test web page is also included in `http://localhost:<port>/webtest.html`
+
+Examples using curl client:
+
+#### Check server
+	curl http://localhost:8000
+
+#### List restaurants
+	curl http://localhost:8000/restaurants
+
+#### Get restaurant details
+	curl http://localhost:8000/restaurants/2
+
+#### Update restaurant details
+	curl -H "Content-type:application/json" -X PUT -d '{"nombre":"Nuevo nombre"}' http://localhost:8000/restaurants/2
+
+#### Create restaurant
+	curl -H "Content-type:application/json" -X POST -d '{"nombre":"Mi restaurante"}' http://localhost:8000/restaurants
+
+#### Delete restaurant
+	curl -X DELETE http://localhost:8000/restaurants/3
---
+++ model.js
@@ -0,0 +1,66 @@
+var MongoClient = require('mongodb').MongoClient;
+var db_url = process.env.DATABASE || 'mongodb://localhost:27017/bio_db';
+
+// Global variable for the database connection
+var db;
+
+// Inits the connection with the database
+exports.init = function (callback) {
+	console.log('Connecting to the database', db_url, '...');
+
+	// Implementar conexión a la base de datos
+	// El objeto de base de datos que devuelve la función de conexión debe asignarse a la variable global 'db'
+	// Una vez conectado correctamente debe llamarse a la función 'callback'
+
+	callback();
+}
+
+// Returns a list of the available restaurants in the database
+exports.list = function (callback, error) {
+	console.log('Getting restaurants list');
+	
+	// Implementar consulta para listar los restaurantes disponibles. 
+	// El array de restaurantes debe devolverse como parámetro de la función callback
+
+	callback();
+}
+
+// Returns the details of a restaurant in the database
+exports.read = function (restaurantId, callback, error) {
+	console.log('Getting restaurant', restaurantId);
+
+	// Implementar consulta para obtener los detalles del restaurante con 'restaurant_id' = restaurantId
+	// El json con los detalles del restaurante debe devolverse como parámetro de la función callback
+
+	callback();
+}
+
+// Creates a new restaurant in the database
+exports.create = function (data, callback, error) {
+	console.log('Creating restaurant with data', data);
+	
+	// Implementar creación de restaurante con los datos contenidos en 'data'. 
+	// Una vez creado satisfactoriamente debe llamarse a la función 'callback'
+
+	callback();
+}
+
+// Updates the details of a restaurant in the database
+exports.update = function (restaurantId, data, callback, error) {
+	console.log('Updating restaurant', restaurantId, 'with data', data);
+	
+	// Implementar actualización de restaurante con los datos contenidos en 'data'. 
+	// Una vez actualizado satisfactoriamente debe llamarse a la función 'callback'
+
+	callback();
+}
+
+// Removes a restaurant from the database
+exports.delete = function (restaurantId, callback, error) {
+	console.log('Deleting restaurant', restaurantId);
+
+	// Implementar borrado de restaurante del restaurante con 'restaurant_id' = restaurantId
+	// Una vez borrado satisfactoriamente debe llamarse a la función 'callback'
+
+	callback();
+}
